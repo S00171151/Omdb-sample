702e8c2d1a8bfe5dfc3da814aba0a5086cecdd28
James Coggins <s00171151@mail.itsligo.ie>
2019-05-04 16:20:12 +0100

    Add base project

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { OmdbApiService } from './services/omdb-api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [OmdbApiService],
+})
+export class AppComponent {
+  title = 'omdb-example';
+  movieData: IOMDBResponse;
+  errorMessage: string;
+  imageHeight = 300;
+
+
+  constructor(private _omdbService: OmdbApiService) {
+
+  }
+
+  getMovieDetails(movieName: string): boolean {
+    this._omdbService.getMovieData(movieName).subscribe(movieData => {
+      this.movieData = movieData;
+      console.log('getMovieDetails: ' + this.movieData);
+    },
+      error => this.errorMessage = <any>error);
+    return false;
+  }
+}
